4876fd31c97f4b3c2956b9a27c78cc3c2fdadc6b
Sandali Weththasinghe <37362261+SandaliWeththasinghe@users.noreply.github.com>
2020-01-16 01:51:06 +0530

    Serial Print

---
+++ Serialprint/Serialprint.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("hello world");
+  delay(2000);
+}
